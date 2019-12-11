f4d27df153a54c6a9c647efe73f7028757b9f940
Jeffery <450788018@qq.com>
2019-12-11 20:17:15 +0800

    修改32位系统无法使用白板的问题

--- app/features/third-part/functions.js
+++ app/features/third-part/functions.js
@@ -4,6 +4,7 @@ import _ from 'lodash';
 import { logger } from '../logger';
 
 let cmd = require('child_process'); //子进程
+const fs = require('fs');
 
 let cmdSubprocess = {};
 
@@ -14,19 +15,45 @@ let cmdSubprocess = {};
  */
 export function openWhiteBoard() {
     logger('openWhiteBoard');  
+    fs.exists('C:\\Program Files\\OpenBoard\\openboard.exe',function(exists){
 
-    cmdSubprocess = cmd.spawn('C:\\Program Files (x86)\\OpenBoard\\openboard.exe');
-    cmdSubprocess.on('close', function(code) {
-        logger('openWhiteBoard exited with code :' + code);
-        cmdSubprocess = {};
-    });
-    cmdSubprocess.stdout.on('data', function(data) {
-        logger('openWhiteBoard stdout: ' + data);
-    });
-    cmdSubprocess.stderr.on('data', function(data) {
-        logger('openWhiteBoard stderr: ' + data);
-
-    });
+            if(exists){
+        
+                    
+            cmdSubprocess = cmd.spawn('C:\\Program Files\\OpenBoard\\openboard.exe');
+            cmdSubprocess.on('close', function(code) {
+                logger('openWhiteBoard exited with code :' + code);
+                cmdSubprocess = {};
+            });
+            cmdSubprocess.stdout.on('data', function(data) {
+                logger('openWhiteBoard stdout: ' + data);
+            });
+            cmdSubprocess.stderr.on('data', function(data) {
+                logger('openWhiteBoard stderr: ' + data);
+        
+            });
+        
+            }
+        
+            else{
+        
+                    
+            cmdSubprocess = cmd.spawn('C:\\Program Files (x86)\\OpenBoard\\openboard.exe');
+            cmdSubprocess.on('close', function(code) {
+                logger('openWhiteBoard exited with code :' + code);
+                cmdSubprocess = {};
+            });
+            cmdSubprocess.stdout.on('data', function(data) {
+                logger('openWhiteBoard stdout: ' + data);
+            });
+            cmdSubprocess.stderr.on('data', function(data) {
+                logger('openWhiteBoard stderr: ' + data);
+        
+            });
+        
+            }
+        
+        });
 
 }
 
